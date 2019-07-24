3d80e19692f0a655c451fc49785e223aa329e243
Bogdan Savluk <savluk.bogdan@gmail.com>
2019-07-24 23:31:59 +0200

    skip files with errors instead of crashing

--- src/cli.ts
+++ src/cli.ts
@@ -39,69 +39,76 @@ async function main(cwd: string, opts: Options) {
 
   for (const file of files) {
     console.log(file);
+    try {
+      let filepath = path.join(cwd, file);
+      const source = fs.readFileSync(filepath, { encoding: 'utf8' });
 
-    let filepath = path.join(cwd, file);
-    const source = fs.readFileSync(filepath, { encoding: 'utf8' });
+      const { isJSX, isFlow } = detectOptions(source, file);
 
-    const { isJSX, isFlow } = detectOptions(source, file);
+      if (!isFlow) {
+        console.log('no flow - skip');
+        continue;
+      }
 
-    console.log({ isFlow, isJSX });
-    if (!isFlow) {
-      continue;
-    }
+      const tsSyntax = babel.transformSync(source, {
+        babelrc: false,
+        filename: filepath,
+        plugins: [...transformPlugins, [tsToFlowPlugin, { isJSX }]],
+      });
 
-    const tsSyntax = babel.transformSync(source, {
-      babelrc: false,
-      filename: filepath,
-      plugins: [...transformPlugins, [tsToFlowPlugin, { isJSX }]],
-    });
+      if (tsSyntax === null) {
+        throw new Error(
+          'babel.transformSync returned null - probably there is some configuration error'
+        );
+      }
 
-    if (tsSyntax === null) {
-      throw new Error(
-        'babel.transformSync returned null - probably there is some configuration error'
+      const targetExt = isJSX
+        ? '.tsx'
+        : /\.js\.flow$/i.test(file)
+        ? '.d.ts'
+        : '.ts';
+
+      const targetFileName = file.replace(/(?:\.jsx?|\.js\.flow)$/i, targetExt);
+      const targetFile = path.join(cwd, targetFileName);
+
+      const ts = babel.transformSync(tsSyntax.code as string, {
+        babelrc: false,
+        filename: targetFile,
+        plugins: [...transformPlugins, tsTypesPlugin],
+      });
+
+      if (ts === null) {
+        throw new Error(
+          'babel.transformSync returned null - probably there is some configuration error'
+        );
+      }
+
+      const prettierConfig = (await prettier.resolveConfig(targetFile)) || {};
+      prettierConfig.parser = 'typescript';
+      const result = prettier.format(ts.code as string, prettierConfig);
+
+      fs.writeFileSync(targetFile, result);
+
+      const verificationResult = verify(
+        source,
+        result,
+        isJSX,
+        filepath,
+        targetFile
       );
-    }
-
-    const targetExt = isJSX
-      ? '.tsx'
-      : /\.js\.flow$/i.test(file)
-      ? '.d.ts'
-      : '.ts';
-
-    const targetFileName = file.replace(/(?:\.jsx?|\.js\.flow)$/i, targetExt);
-    const targetFile = path.join(cwd, targetFileName);
-
-    const ts = babel.transformSync(tsSyntax.code as string, {
-      babelrc: false,
-      filename: targetFile,
-      plugins: [...transformPlugins, tsTypesPlugin],
-    });
-
-    if (ts === null) {
-      throw new Error(
-        'babel.transformSync returned null - probably there is some configuration error'
-      );
-    }
-
-    const prettierConfig = (await prettier.resolveConfig(targetFile)) || {};
-    prettierConfig.parser = 'typescript';
-    const result = prettier.format(ts.code as string, prettierConfig);
-
-    fs.writeFileSync(targetFile, result);
-
-    const verificationResult = verify(
-      source,
-      result,
-      isJSX,
-      filepath,
-      targetFile
-    );
-    if (verificationResult.isEqual) {
-      console.log(
-        'verification failed, diff after stripping type annotations:'
-      );
-      const changes = jestDiff(verificationResult.src, verificationResult.tgt);
-      console.log(changes);
+      if (verificationResult.isEqual) {
+        console.log(
+          'verification failed, diff after stripping type annotations:'
+        );
+        const changes = jestDiff(
+          verificationResult.src,
+          verificationResult.tgt
+        );
+        console.log(changes);
+      }
+    } catch (e) {
+      console.error('error while trying to convert');
+      console.error(e);
     }
   }
 }
