26c4e9c0c53e92802ab6d6dc68787b394187d046
Bogdan Savluk <savluk.bogdan@gmail.com>
2020-03-19 01:17:32 +0100

    babel 7.9.0, dependencies update

--- test/visitors/optional_member_expression.test.ts
+++ test/visitors/optional_member_expression.test.ts
@@ -3,27 +3,26 @@ import plugin from '../../src';
 
 pluginTester({
   plugin,
-  // todo: it looks there is a bug in babel resulting in additional parenthesis when generating code with optional chaining
   tests: [
     {
       title: 'optional member expression',
       code: `console.log(a?.b);`,
-      output: `console.log((a?.b));`,
+      output: `console.log(a?.b);`,
     },
     {
       title: 'optional multi member expression',
       code: `console.log(a?.b?.c);`,
-      output: `console.log((a?.b?.c));`,
+      output: `console.log(a?.b?.c);`,
     },
     {
       title: 'optional multi mixed member expression',
       code: `console.log(a.b?.c);`,
-      output: `console.log((a.b?.c));`,
+      output: `console.log(a.b?.c);`,
     },
     {
       title: 'Optional numeral literal access',
       code: `console.log(a?.[0]?.c);`,
-      output: `console.log((a?.[0]?.c));`,
+      output: `console.log(a?.[0]?.c);`,
     },
   ],
 });
