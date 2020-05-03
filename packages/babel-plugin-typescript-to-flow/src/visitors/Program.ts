176d7aadaa7c706f8095108d9305e3efd8bba24a
Bogdan Savluk <savluk.bogdan@gmail.com>
2020-05-03 20:45:07 +0200

    improve class declaration handling, add ambient mode support

--- packages/babel-plugin-typescript-to-flow/src/visitors/Program.ts
+++ packages/babel-plugin-typescript-to-flow/src/visitors/Program.ts
@@ -2,6 +2,7 @@ import * as t from '@babel/types';
 import { NodePath } from '@babel/traverse';
 import { replaceWith } from '../utils/replaceWith';
 import { convertTSTypeAliasDeclaration } from '../converters/convertTSTypeAliasDeclaration';
+import { transformClassDeclaration } from '../transforms/transformClassDeclaration';
 
 export function Program(path: NodePath<t.Program>) {
   // todo: pass this in plugin options
@@ -12,6 +13,13 @@ export function Program(path: NodePath<t.Program>) {
     if (t.isTSTypeAliasDeclaration(node)) {
       const replacement = convertTSTypeAliasDeclaration(node, isAmbientContext);
       replaceWith(st, replacement);
+    }
+    // todo: isClassDeclaration on NodePath
+    if (t.isClassDeclaration(node)) {
+      transformClassDeclaration(
+        st as NodePath<t.ClassDeclaration>,
+        isAmbientContext
+      );
     }
   }
 }
