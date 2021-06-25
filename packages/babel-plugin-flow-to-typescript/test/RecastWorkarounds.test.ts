e2da957eecd50992676ae1bf63748b3c7539c64f
Bogdan Savluk <savluk.bogdan@gmail.com>
2021-06-25 19:44:53 +0200

    update recast plugin fixing issue with whitespace in template string, add test for it

--- packages/babel-plugin-flow-to-typescript/test/RecastWorkarounds.test.ts
+++ packages/babel-plugin-flow-to-typescript/test/RecastWorkarounds.test.ts
@@ -67,3 +67,65 @@ export const StyledTableHeadCell = styled<StyledTableHeadCellPropsT>(
     });"
   `);
 });
+
+describe('whitespace', () => {
+  test('preserve tabs mixes with spaces template strings', () => {
+    const result = testTransform(`
+// @flow
+// @flow
+
+const test = \`
+    {
+    	from: {
+        	service: 'p',
+          idlService: 'p',
+      },\`;
+`);
+    expect(result.babel).toMatchInlineSnapshot(`
+"//
+const test = \`
+    {
+    	from: {
+        	service: 'p',
+          idlService: 'p',
+      },\`;"
+`);
+    expect(result.recast).toMatchInlineSnapshot(`
+"//
+
+const test = \`
+    {
+    	from: {
+        	service: 'p',
+          idlService: 'p',
+      },\`;"
+`);
+  });
+
+  /* eslint-disable no-irregular-whitespace */
+  test('preserve NBSP in template strings', () => {
+    const result = testTransform(`
+// @flow
+
+expect(wrapper).toMatchInlineSnapshot(\`
+  <Fragment>
+     
+  </Fragment>
+\`);
+`);
+    expect(result.babel).toMatchInlineSnapshot(`
+"expect(wrapper).toMatchInlineSnapshot(\`
+  <Fragment>
+     
+  </Fragment>
+\`);"
+`);
+    expect(result.recast).toMatchInlineSnapshot(`
+"expect(wrapper).toMatchInlineSnapshot(\`
+  <Fragment>
+     
+  </Fragment>
+\`);"
+`);
+  });
+});
